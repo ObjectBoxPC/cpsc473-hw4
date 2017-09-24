1fd137edaaf2d2eb94f3d15c91a7c54ca0c63fec
Philip Chung <philipchung1995@yahoo.com>
2017-09-24 01:04:51 -0700

    Add test files

---
+++ tests/test_datastore.js
@@ -0,0 +1,8 @@
+var ds = new App.DataStore();
+ds.add('m@bond.com', 'tea');
+ds.add('james@bond.com', 'eshpressho');
+console.log(ds.getAll());
+ds.remove('james@bond.com');
+console.log(ds.getAll());
+console.log(ds.get('m@bond.com'));
+console.log(ds.get('james@bond.com'));
